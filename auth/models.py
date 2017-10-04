35248d2babc21348ed47af81e4e46d7ad9579740
root <root@localhost.localdomain>
2017-10-04 21:02:28 +0800

    create project and app.

---
+++ auth/models.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    userName = models.CharField(max_length=45, unique=True)
+    nickName = models.CharField(max_length=45, unique=True)
+    email = models.CharField(max_length=60, unique=True)
+    phoneNum = models.CharField(max_length=30, unique=True)
+    comment = models.TextField(max_length=100, null=True)
+    createTimestamp = models.DateTimeField(auto_now_add=True)
+    updateTimestamp = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'user'
+    def __unicode__(self):
+        return u'%d %s %s %s %s' % (self.id, 
+                                    self.userName, 
+                                    self.nickName, 
+                                    self.email, 
+                                    self.phoneNum, 
+                                    self.comment)
+
+class LocalAuth(models.Model):
+    id = models.AutoField(primary_key=True)
+    uid = models.IntegerField(unique=True)
+    salt = models.CharField(max_length=45)
+    password = models.CharField(max_length=256)
+    createTimestamp = models.DateTimeField(auto_now_add=True)
+    updateTimestamp = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'localAuth'
+    def __unicode__(self):
+        return u'%d %s %s' % (self.uid, self.salt, self.password)
+
+class Token(models.Model):
+    id = models.AutoField(primary_key=True)
+    uid = models.IntegerField(unique=True)
+    token = models.TextField(max_length=100, unique=True)
+    permission = models.TextField(max_length=280)
+    ip = models.CharField(max_length=15)
+    createTimestamp = models.DateTimeField(auto_now_add=True)
+    updateTimestamp = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'token'
+        unique_together = (("uid", "token"))
+    def __unicode__(self):
+        return u'%d %d %s %s' % (self.id, self.uid, self.token, self.permissions)
+
+class Role(models.Model):
+    id = models.AutoField(primary_key=True)
+    roleName = models.CharField(max_length=45)
+    comment = models.TextField(max_length=100)
+    createTimestamp = models.DateTimeField(auto_now_add=True)
+    updateTimestamp = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'role'
+    def __unicode__(self):
+        return u'%d %s %s' % (self.rid, self.roleName, self.comment)
+
+class RoleMember(models.Model):
+    id = models.AutoField(primary_key=True)
+    uid = models.IntegerField(unique=True)
+    rid = models.IntegerField(unique=True)
+    createTimestamp = models.DateTimeField(auto_now_add=True)
+    updateTimestamp = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'roleMember'
+    def __unicode__(self):
+        return u'%d %d %d' % (self.id, self.uid, self.rid)
+
+class Module(models.Model):
+    id = models.AutoField(primary_key=True)
+    moduleName = models.CharField(max_length=45)
+    nickName = models.CharField(max_length=45)
+    comment = models.TextField(max_length=100)
+    createTimestamp = models.DateTimeField(auto_now_add=True)
+    updateTimestamp = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'module'
+    def __unicode__(self):
+        return u'%d %s %s %s' % (
+                self.id, 
+                self.moduleName, 
+                self.nickName, 
+                self.comment
+                )
+
+class Permission(models.Model):
+    id = models.AutoField(primary_key=True)
+    mid = models.IntegerField(unique=True)
+    rid = models.IntegerField(unique=True)
+    permission = models.IntegerField()
+    createTimestamp = models.DateTimeField(auto_now_add=True)
+    updateTimestamp = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'permission'
+    def __unicode__(self):
+        return u'%d %d %d' % (self.mid, self.rid, self.permission)
+
+class ModuleRelationship(models.Model):
+    id = models.AutoField(primary_key=True)
+    mid = models.IntegerField(unique=True)
+    pid = models.IntegerField(unique=True)
+    createTimestamp = models.DateTimeField(auto_now_add=True)
+    updateTimestamp = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'moduleRelationship'
+    def __unicode__(self):
+        return u'%d %d %d' % (self.mid, self.rid, self.permission)
---
+++ setup/create_db.sql
@@ -0,0 +1,102 @@
+#change user pass:
+#SET PASSWORD FOR 'omcenter'@'%' = PASSWORD("123456");
+
+#create user omcenter identified by 'OmCenter';
+#create user 'omcenter'@'localhost' identified by 'OmCenter';
+#create user 'omcenter'@'127.0.0.1' identified by 'OmCenter';
+#create user 'omcenter'@'%' identified by 'OmCenter';
+#create database omcenter;
+#grant all on omcenter.* to 'omcenter'@'%';
+#grant all on omcenter.* to 'omcenter'@'localhost';
+#grant all on omcenter.* to 'omcenter'@'127.0.0.1';
+
+#auth models
+
+create table user (
+    id int not null AUTO_INCREMENT,
+    userName varchar(45) not null unique,
+    nickName varchar(45) not null unique,
+    email varchar(60) not null unique,
+    phoneNum varchar(30) not null unique,
+    comment text default null,
+    createTimestamp DATETIME,
+    updateTimestamp DATETIME,
+    primary key(id)
+    );
+
+create table localAuth (
+    id int not null AUTO_INCREMENT,
+    uid int NOT NULL unique,
+    salt varchar(45) not null,
+    password varchar(256) not null,
+    createTimestamp DATETIME,
+    updateTimestamp DATETIME,
+    primary key(id),
+    foreign key (uid) references user (id)
+    );
+
+create table token (
+    id int not null AUTO_INCREMENT,
+    uid int not null,
+    token varchar(100) not null,
+    permission text not null,
+    ip varchar(15) not null,
+    createTimestamp DATETIME,
+    updateTimestamp DATETIME,
+    primary key(id),
+    unique key token(uid, token),
+    foreign key (uid) references user (id)
+    );
+
+create table role (
+    id int NOT NULL AUTO_INCREMENT,
+    role_name varchar(45) not null unique,
+    comment text default null,
+    createTimestamp DATETIME,
+    updateTimestamp DATETIME,
+    primary key(id)
+    );
+
+create table roleMember (
+    id int NOT NULL AUTO_INCREMENT,
+    uid int not null,
+    rid int not null,
+    createTimestamp DATETIME,
+    updateTimestamp DATETIME,
+    primary key(id),
+    foreign key (uid) references user (id),
+    foreign key (rid) references role (id)
+    );
+
+create table module (
+    id int not null AUTO_INCREMENT,
+    moduleName varchar(32) not null unique,
+    nickName varchar(32) not null,
+    comment text default null,
+    createTimestamp DATETIME,
+    updateTimestamp DATETIME,
+    primary key(id)
+    );
+
+create table permission (
+    id int not null AUTO_INCREMENT,
+    mid int not null,
+    rid int not null,
+    permission int not null,
+    createTimestamp DATETIME,
+    updateTimestamp DATETIME,
+    primary key(id),
+    foreign key (mid) references module (id),
+    foreign key (rid) references role (id)
+    );
+
+create table moduleRelationship (
+    id int not null AUTO_INCREMENT,
+    mid int not null,
+    pid int not null,
+    createTimestamp DATETIME,
+    updateTimestamp DATETIME,
+    primary key(id),
+    foreign key (mid) references module (id),
+    foreign key (pid) references module (id)
+    );
